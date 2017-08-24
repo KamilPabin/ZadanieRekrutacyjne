0218dfcf721929021d7ef1e5d2828fe73e882712
KamilPabin <pabin.kamil@gmail.com>
2017-08-24 17:41:58 +0200

    Initial commit

---
+++ src/main/java/DataModels/EventOnRoad.java
@@ -0,0 +1,18 @@
+package DataModels;
+
+/**
+ * Created by Little on 2017-08-24.
+ */
+public class EventOnRoad {
+
+    private double longitude;
+    private double latitude;
+
+    private String name;
+    private String description;
+
+    private String type;
+
+    
+
+}
