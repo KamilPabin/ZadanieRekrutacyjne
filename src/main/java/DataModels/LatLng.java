f1ca663e1b2427d0a8c4b678ca7117af7490e06e
KamilPabin <pabin.kamil@gmail.com>
2017-08-31 12:47:39 +0200

    Update 10
    Dodano obliczanie odległości do wydarzenia po stronie serwera

--- src/main/java/DataModels/LatLng.java
+++ src/main/java/DataModels/LatLng.java
@@ -24,8 +24,18 @@ public class LatLng {
         this.longitude = longitude;
     }
 
-    public double distanceTo(LatLng point){
+    public double distanceTo(LatLng point) {
+        double R = 6371;
+        double dLat = Math.toRadians(latitude - point.latitude);
+        double dLon = Math.toRadians(longitude - point.longitude);
+        double a =
+                Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(Math.toRadians(latitude)) * Math.cos(Math.toRadians(point.latitude)) *
+                Math.sin(dLon / 2) * Math.sin(dLon / 2);
 
-        return 0.0;
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double d = R * c * 1000;
+        System.out.println(d);
+        return d;
     }
 }
